cf6ac07fdd0b3b85caea986690eaa7cfd2568199
Aisha Shaheed <ps.ashaheed@outlook.com>
2025-12-12 00:58:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -93,20 +93,85 @@ for (let i = 0; i < csv.length; i++) {
 // // print individual cells
 
 // Starting string - input
-const str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
+/*const str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
+
+//Arrays for storing new data
+// const rows =[]; //for the parsed row data - final destination
+let rowData=[]; //for creating the rows with the cellData.
+let cellData = "";//for storing iterized cell data. It will keep track of commas and "/n" as it goes through the string.
+
+
+// Loop through each character in the CSV string
+
+
+for(i=0; i<str.length; i++) {
+  //Saving value current char at the current index into a variable to make easier to use.
+  const current = str[i]; //instead of writing str[i] all the time. So, variable current will sift through each cell's data.
+
+  if (current === ",") {//when current or str[i] reaches a comma, it iterates to the next cell
+    // Move to the next cell
+  rowData.push(cellData); //iteration past commas
+  cellData="";
+  
+   
+} else if (current === "\n") {//iteration past the end of the row, which is indicated by "\n".
+   rowData.push(cellData);
+        // rows.push(rowData);
+        rowData = [];
+        cellData = "";
+  } else {cellData+=current;
+    // For all other chars
+    // Add data to a cell
+
+    if (cellData.length>0) { // If there is 0 data then this is the end of the loop
+      rowData.push(cellData);
+    // rows.push(rowData);
+    }
+  }
+
+  //splice const rows so that header(categories) are renoved
+
+  // const headers = rows.splice(0,4);
+console.log(cellData);
+// Display the parsed result
+// console.log("All rows:", rows);
+// console.log("Categories:", headers);
+
+   
+  }*/
+// Part 1
+// ****************************************************************************
 
 //add one row of data from a csv list to an array
 
-const headings = str.split(" ");//organizes the data by space in between the words, not characters.
+/*const str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
 
-console.log(headings); // I am not sure why there is a (4) there
+const headings = str.split(" ");//removes the space between the list of items
+
+console.log(headings); // I don't why that 4 appears.
+*/
 
 
 
-    
-  
+  // ****************************************************************************
+
   //Part 2 Expanding Functionality
   
+
+let columnNum = `ID,Name,Occupation,Age
+42,Bruce,Knight,41
+57,Bob,Fry Cook,19
+63,Blaine,Quiz Master,58
+98,Bill,Doctor’s Assistant,26`;
+const rows=[];
+const dataRows = rows.slice(1); //this returns the first row into the dataRows array
+let headings = rows[0];//the headings start withthe first item in the row, which is ID.
+for (i=0; i<columnNum.length; i++);{ //calculating whatever the length is of the columns in the csv.
+  console.log(`Column ${i+1}: `);
+  for(let k=0; k<headings.length; k++){//iteration through the headings variable
+    console.log(`$headings[k]}:${columnNum[i][k]}`);
+  }
+}
 
 
 
